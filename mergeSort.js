2fa4ea996501e0035f569b90c8b269a52e5d3b2c
Leon vdb <leon.vondemberge@gmail.com>
2019-09-03 13:44:30 +0300

    merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -19,4 +19,13 @@ const merge = (arrA, arrB) => {
     return mergedArray
 }
 
-console.log(merge([2, 4, 10, 12, 14, 16, 33, 240, 330, 331, 332, 333], [1, 2, 3, 5, 6, 7, 8, 9]))
+// console.log(merge([2, 4, 10, 12, 14, 16, 33, 240, 330, 331, 332, 333], [1, 2, 3, 5, 6, 7, 8, 9]))
+
+const mergeSort = (arr) => {
+    if (arr.length <= 1) return arr
+    const left = mergeSort(arr.slice(0, Math.round(arr.length / 2)))
+    const right = mergeSort(arr.slice(Math.round(arr.length / 2)))
+    return merge(left, right)
+}
+
+console.log(mergeSort([4, 1, 5, 2, 12, 2, 3, 7, 24, 290, 6, 12, 3]))
